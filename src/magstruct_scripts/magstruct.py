097177984ea9bdca19a08f911f5741d03353a343
Rosnec <rosnec.dev@gmail.com>
2014-06-30 18:33:58 +0530

    Wrote beginning of script. It's not working, though.

--- src/magstruct_scripts/magstruct.py
+++ src/magstruct_scripts/magstruct.py
@@ -1,6 +1,8 @@
 from argparse import ArgumentError, ArgumentParser, FileType
 from magstruct import plane, transformations
-from numpy import float as npfloat
+from numpy import array, float as npfloat, loadtxt, savetxt
+from os import path
+import matplotlib.pyplot as plt
 
 def get_args():
     parser = ArgumentParser()
@@ -17,8 +19,19 @@ def get_args():
     return args
 
 def main():
-    print("Hello")
+    args = get_args()
 
+    ra_dec_dist = loadtxt(args.input)
+    xyz = array([transformations.radec2xyz(rdd, args.center)
+                 for rdd in ra_dec_dist])
+    savetxt(path.join(args.output, "cartesian.dat"), xyz)
+    x, y, z = xyz.T
+    plt.scatter(x, z)
+    plt.savefig(path.join(args.output, "cartesian.png"))
+    plt.clf()
+    ra, dec, dist = ra_dec_dist.T
+    plt.scatter(ra, dec)
+    plt.savefig(path.join(args.output, "radec.png"))
 
 if __name__ == '__main__':
     exit(main())
